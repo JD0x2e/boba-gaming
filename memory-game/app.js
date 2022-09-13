7bc8bf9f361b49dc30654a6599913dc3a97def91
Jack Doy <jackdoy99@icloud.com>
2022-09-13 16:44:59 +0100

    Updated everything

---
+++ memory-game/app.js
@@ -0,0 +1,116 @@
+let counter = 0;
+let firstCardSelection = "";
+let secondCardSelection = "";
+let firstCardId = 0;
+let secondCardId = 0;
+let cardsContainer = document.getElementById("cards");
+
+const carNames = [
+  { name: "Red Bull", src: "./images/Redbull.png" },
+  { name: "Mercedes", src: "./images/Mercedes.png" },
+  { name: "Ferrari", src: "./images/Ferrari.png" },
+  { name: "Mclaren", src: "./images/Mclaren.png" },
+  { name: "Alpha Tauri", src: "./images/Alphatauri.png" },
+  { name: "Aston Martin ", src: "./images/Astonmartin.png" },
+  { name: "Alpine", src: "./images/Alpine.png" },
+  { name: "Alfa Romeo", src: "./images/Alfaromeo.png" },
+  { name: "Haas", src: "./images/Haas.png" },
+  { name: "Williams", src: "./images/Williams.png" },
+];
+
+function Card(src, name, id) {
+  this.src = src;
+  this.name = name;
+  this.id = id;
+  Card.allCars.push(this);
+}
+
+Card.prototype.render = function () {
+  let flipBox = document.createElement("div");
+  flipBox.classList.add("flip-box");
+  flipBox.id = this.id;
+
+  let flipBoxInner = document.createElement("div");
+  flipBoxInner.classList.add("flip-box-inner");
+  flipBox.appendChild(flipBoxInner);
+
+  let flipBoxFront = document.createElement("div");
+  flipBoxFront.classList.add("flip-box-front");
+  flipBoxInner.appendChild(flipBoxFront);
+
+  let flipBoxBack = document.createElement("div");
+  flipBoxBack.classList.add("flip-box-back");
+  flipBoxInner.appendChild(flipBoxBack);
+
+  let carImg = document.createElement("img");
+  carImg.src = this.src;
+  carImg.alt = this.name;
+  flipBoxBack.appendChild(carImg);
+
+  let logoImg = document.createElement("img");
+  logoImg.src = "./images/bobagaminglogonobg.png";
+  logoImg.alt = "Boba Gaming Logo";
+  flipBoxFront.appendChild(logoImg);
+
+  const myObj = this;
+
+  flipBox.addEventListener("click", function () {
+    flipBox.classList.add("clicked");
+    console.log(myObj);
+    if (counter === 0) {
+      firstCardSelection = myObj.name;
+      counter++;
+      firstCardId = myObj.id;
+    } else {
+      secondCardSelection = myObj.name;
+      counter = 0;
+      secondCardId = myObj.id;
+
+      let firstCard = document.getElementById(firstCardId);
+
+      if (
+        firstCardSelection === secondCardSelection &&
+        firstCardId != secondCardId
+      ) {
+        console.log("snap");
+        flipBox.classList.add("matched");
+        firstCard.classList.add("matched");
+      } else {
+        setTimeout(function () {
+          flipBox.classList.remove("clicked");
+          firstCard.classList.remove("clicked");
+        }, 2000);
+        // set time out so classes get removed after 2seconds
+      }
+
+      firstCardSelection = "";
+      secondCardSelection = "";
+      firstCardId = 0;
+      secondCardId = 0;
+    }
+  });
+
+  cardsContainer.appendChild(flipBox);
+};
+
+Card.allCars = [];
+
+let currentId = 1;
+
+for (let i = 0; i < carNames.length; i++) {
+  let newCard = new Card(carNames[i].src, carNames[i].name, currentId);
+  currentId++;
+  let newCardCopy = new Card(carNames[i].src, carNames[i].name, currentId);
+  currentId++;
+  newCard.render();
+  newCardCopy.render();
+}
+
+console.log(Card.allCars);
+
+console.log(firstCardSelection);
+console.log(secondCardSelection);
+
+// when click car, save that name in an array, and when you click a second one, check it against the first one, if it matches then change the card color to green.
+
+// make another function which loops through the allcars array and then render the cards in that
