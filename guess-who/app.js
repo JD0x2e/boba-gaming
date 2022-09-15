3ae6ff440fab0cbe908fcc119e7b4184aee75288
Chris Seaman <christopherseaman1983@gmail.com>
2022-09-15 13:29:12 +0100

    app.js updated local storage to render score across page refresh

--- guess-who/app.js
+++ guess-who/app.js
@@ -1,246 +1,252 @@
-const hideBtn1 = document.getElementById("hide-btn1");
-const hideBtn2 = document.getElementById("hide-btn2");
-const hideBtn3 = document.getElementById("hide-btn3");
-const hideBtn4 = document.getElementById("hide-btn4");
-const hideBtn5 = document.getElementById("hide-btn5");
-const hideBtn6 = document.getElementById("hide-btn6");
-const hideBtn7 = document.getElementById("hide-btn7");
-const hideBtn8 = document.getElementById("hide-btn8");
-const hideBtn9 = document.getElementById("hide-btn9");
-const hideBtn10 = document.getElementById("hide-btn10");
-const hideBtn11 = document.getElementById("hide-btn11");
-const hideBtn12 = document.getElementById("hide-btn12");
-const showBtn1 = document.getElementById("show-btn1");
-const showBtn2 = document.getElementById("show-btn2");
-const showBtn3 = document.getElementById("show-btn3");
-const showBtn4 = document.getElementById("show-btn4");
-const showBtn5 = document.getElementById("show-btn5");
-const showBtn6 = document.getElementById("show-btn6");
-const showBtn7 = document.getElementById("show-btn7");
-const showBtn8 = document.getElementById("show-btn8");
-const showBtn9 = document.getElementById("show-btn9");
-const showBtn10 = document.getElementById("show-btn10");
-const showBtn11 = document.getElementById("show-btn11");
-const showBtn12 = document.getElementById("show-btn12");
+let guessWhoScore = [];
 
+const scoresFromLocalStorage = JSON.parse(
+  localStorage.getItem("guessWhoScore")
+);
+if (scoresFromLocalStorage) {
+  guessWhoScore = scoresFromLocalStorage;
+}
+
+const scoreContainer = document.getElementById("score-container");
+
+function renderGWPlayerScore() {
+  let guessWhoWin = 0;
+  let guessWhoTotal = 0;
+  for (i = 0; i < guessWhoScore.length; i++) {
+    guessWhoTotal++;
+    if (guessWhoScore[i] === "win") {
+      guessWhoWin++;
+    }
+  }
+  let headingWin = document.createElement("h5");
+  headingWin.style.lineHeight = 1.6;
+  headingWin.style.color = "rgb(250, 165, 73)";
+  headingWin.innerHTML = `Your Score <br /> ${guessWhoWin} / ${guessWhoTotal}`;
+  scoreContainer.appendChild(headingWin);
+}
+
+const characterContainer = document.getElementById("characterContainer");
 const question = document.getElementById("clue");
 const answerHeading = document.getElementById("answer-heading");
 const selectGuess = document.getElementById("guess");
+const guessResponse = document.getElementById("guessResponse");
 
-function Character(
-  name,
-  src,
-  shadow,
-  emotion,
-  skin,
-  hair,
-  nose,
-  eyes,
-  lips,
-  facialHair,
-  headWear,
-  hide,
-  show
-) {
-  this.name = name;
-  this.src = src;
-  this.shadow = shadow;
-  this.emotion = emotion;
-  this.skin = skin;
-  this.hair = hair;
-  this.nose = nose;
-  this.eyes = eyes;
-  this.lips = lips;
-  this.facialHair = facialHair;
-  this.headWear = headWear;
-  this.hide = hide;
-  this.show = show;
+const maxGuessesAllowed = 3;
+let guesses = 0;
+
+function Character(charObj) {
+  this.name = charObj.name;
+  this.src = charObj.src;
+  this.shadow = charObj.shadow;
+  this.emotion = charObj.emotion;
+  this.skin = charObj.skin;
+  this.hair = charObj.hair;
+  this.nose = charObj.nose;
+  this.eyes = charObj.eyes;
+  this.lips = charObj.lips;
+  this.facialHair = charObj.facialHair;
+  this.headWear = charObj.headWear;
+  this.hide = charObj.hide;
+  this.show = charObj.show;
 
   Character.allCharactersArray.push(this);
 }
 
+Character.prototype.render = function () {
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  let image = document.createElement("img");
+  image.classList.add("img-char");
+  image.src = this.src;
+  let heading = document.createElement("h3");
+  heading.classList.add("name-heading");
+  heading.textContent = this.name;
+  let buttonX = document.createElement("button");
+  buttonX.classList.add("hide-btn");
+  buttonX.textContent = "x";
+  let buttonTick = document.createElement("button");
+  buttonTick.innerHTML = "&#10003";
+  characterContainer.appendChild(imgContainer);
+  imgContainer.appendChild(image);
+  imgContainer.appendChild(heading);
+  imgContainer.appendChild(buttonX);
+  imgContainer.appendChild(buttonTick);
+
+  const myObj = this;
+
+  buttonX.addEventListener("click", () => {
+    image.src = myObj.shadow;
+  });
+
+  buttonTick.addEventListener("click", () => {
+    image.src = myObj.src;
+  });
+};
+
 Character.allCharactersArray = [];
 
-new Character(
-  "anita",
-  "/images/anita.png",
-  "/images/anita-shadow.png",
-  "happy",
-  "light",
-  "light",
-  "small",
-  "blue",
-  "small",
-  false,
-  true,
-  "hide-btn1",
-  "show-btn1"
-);
-new Character(
-  "robert",
-  "/images/robert.png",
-  "/images/robert-shadow.png",
-  "sad",
-  "light",
-  "dark",
-  "big",
-  "blue",
-  "big",
-  false,
-  false,
-  "hide-btn2",
-  "show-btn2"
-);
-new Character(
-  "george",
-  "/images/george.png",
-  "/images/george-shadow.png",
-  "sad",
-  "light",
-  "light",
-  "small",
-  "brown",
-  "big",
-  false,
-  true,
-  "hide-btn3",
-  "show-btn3"
-);
-new Character(
-  "alex",
-  "/images/alex.png",
-  "/images/alex-shadow.png",
-  "happy",
-  "dark",
-  "dark",
-  "small",
-  "brown",
-  "big",
-  true,
-  false,
-  "hide-btn4",
-  "show-btn4"
-);
-new Character(
-  "alfred",
-  "/images/alfred.png",
-  "/images/alfred-shadow.png",
-  "happy",
-  "light",
-  "dark",
-  "big",
-  "blue",
-  "small",
-  true,
-  false,
-  "hide-btn5",
-  "show-btn5"
-);
-new Character(
-  "anne",
-  "/images/anne.png",
-  "/images/anne-shadow.png",
-  "happy",
-  "light",
-  "dark",
-  "big",
-  "blue",
-  "small",
-  false,
-  true,
-  "hide-btn6",
-  "show-btn6"
-);
-new Character(
-  "bernard",
-  "/images/bernard.png",
-  "/images/bernard-shadow.png",
-  "happy",
-  "light",
-  "dark",
-  "big",
-  "brown",
-  "small",
-  false,
-  true,
-  "hide-btn7",
-  "show-btn7"
-);
-new Character(
-  "bill",
-  "/images/bill.png",
-  "/images/bill-shadow.png",
-  "happy",
-  "light",
-  "dark",
-  "small",
-  "blue",
-  "small",
-  true,
-  false,
-  "hide-btn8",
-  "show-btn8"
-);
-new Character(
-  "charles",
-  "/images/charles.png",
-  "/images/charles-shadow.png",
-  "happy",
-  "dark",
-  "light",
-  "small",
-  "blue",
-  "big",
-  true,
-  false,
-  "hide-btn9",
-  "show-btn9"
-);
-new Character(
-  "claire",
-  "/images/claire.png",
-  "/images/claire-shadow.png",
-  "happy",
-  "light",
-  "dark",
-  "small",
-  "blue",
-  "small",
-  false,
-  true,
-  "hide-btn10",
-  "show-btn10"
-);
-new Character(
-  "david",
-  "/images/david.png",
-  "/images/david-shadow.png",
-  "happy",
-  "light",
-  "light",
-  "small",
-  "brown",
-  "big",
-  true,
-  false,
-  "hide-btn11",
-  "show-btn11"
-);
-new Character(
-  "eric",
-  "/images/eric.png",
-  "/images/eric-shadow.png",
-  "happy",
-  "light",
-  "light",
-  "small",
-  "blue",
-  "big",
-  false,
-  true,
-  "hide-btn12",
-  "show-btn12"
-);
+const characterDetails = [
+  {
+    name: "anita",
+    src: "/images/anita.png",
+    shadow: "/images/anita-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "blonde",
+    nose: "small",
+    eyes: "blue",
+    lips: "small",
+    facialHair: false,
+    headWear: true,
+  },
+  {
+    name: "robert",
+    src: "/images/robert.png",
+    shadow: "/images/robert-shadow.png",
+    emotion: "sad",
+    skin: "light",
+    hair: "brown",
+    nose: "big",
+    eyes: "blue",
+    lips: "big",
+    facialHair: false,
+    headWear: false,
+  },
+  {
+    name: "george",
+    src: "/images/george.png",
+    shadow: "/images/george-shadow.png",
+    emotion: "sad",
+    skin: "light",
+    hair: "grey",
+    nose: "small",
+    eyes: "brown",
+    lips: "big",
+    facialHair: false,
+    headWear: true,
+  },
+  {
+    name: "alex",
+    src: "/images/alex.png",
+    shadow: "/images/alex-shadow.png",
+    emotion: "happy",
+    skin: "dark",
+    hair: "brown",
+    nose: "small",
+    eyes: "brown",
+    lips: "big",
+    facialHair: true,
+    headWear: false,
+  },
+  {
+    name: "alfred",
+    src: "/images/alfred.png",
+    shadow: "/images/alfred-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "ginger",
+    nose: "big",
+    eyes: "blue",
+    lips: "small",
+    facialHair: true,
+    headWear: false,
+  },
+  {
+    name: "anne",
+    src: "/images/anne.png",
+    shadow: "/images/anne-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "brown",
+    nose: "big",
+    eyes: "blue",
+    lips: "small",
+    facialHair: false,
+    headWear: true,
+  },
+  {
+    name: "bernard",
+    src: "/images/bernard.png",
+    shadow: "/images/bernard-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "brown",
+    nose: "big",
+    eyes: "brown",
+    lips: "small",
+    facialHair: false,
+    headWear: true,
+  },
+  {
+    name: "bill",
+    src: "/images/bill.png",
+    shadow: "/images/bill-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "ginger",
+    nose: "small",
+    eyes: "blue",
+    lips: "small",
+    facialHair: true,
+    headWear: false,
+  },
+  {
+    name: "charles",
+    src: "/images/charles.png",
+    shadow: "/images/charles-shadow.png",
+    emotion: "happy",
+    skin: "dark",
+    hair: "blonde",
+    nose: "small",
+    eyes: "blue",
+    lips: "big",
+    facialHair: true,
+    headWear: false,
+  },
+  {
+    name: "david",
+    src: "/images/david.png",
+    shadow: "/images/david-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "blonde",
+    nose: "small",
+    eyes: "brown",
+    lips: "big",
+    facialHair: true,
+    headWear: false,
+  },
+  {
+    name: "eric",
+    src: "/images/eric.png",
+    shadow: "/images/eric-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "blonde",
+    nose: "small",
+    eyes: "blue",
+    lips: "big",
+    facialHair: false,
+    headWear: true,
+  },
+  {
+    name: "claire",
+    src: "/images/claire.png",
+    shadow: "/images/claire-shadow.png",
+    emotion: "happy",
+    skin: "light",
+    hair: "ginger",
+    nose: "small",
+    eyes: "blue",
+    lips: "big",
+    facialHair: false,
+    headWear: true,
+  },
+];
+
+for (let i = 0; i < characterDetails.length; i++) {
+  new Character(characterDetails[i]);
+}
 
 function getGuessWhoCharacter() {
   return Math.floor(Math.random() * Character.allCharactersArray.length);
@@ -248,17 +254,42 @@ function getGuessWhoCharacter() {
 
 const guessWho = getGuessWhoCharacter();
 
-document.getElementById("question-btn").addEventListener("click", renderAnswer);
+const qBtn = document.getElementById("question-btn");
+qBtn.addEventListener("click", renderAnswer);
 
 function renderAnswer() {
   if (question.value === "") {
     alert("Please select a question to ask!");
   } else if (question.value === "hair-color") {
-    answerHeading.textContent = `The character has ${Character.allCharactersArray[guessWho].hair} hair`;
+    answerHeading.textContent = `The character has ${Character.allCharactersArray[guessWho].hair} hair.`;
   } else if (question.value === "headwear") {
-    answerHeading.textContent = `This is ${Character.allCharactersArray[guessWho].headWear}.`;
+    if (Character.allCharactersArray[guessWho].headWear) {
+      answerHeading.textContent = `The character is wearing at least one item`;
+    } else {
+      answerHeading.textContent = `The character is not wearing any items`;
+    }
+  } else if (question.value === "skin") {
+    answerHeading.textContent = `The character has ${Character.allCharactersArray[guessWho].skin} skin`;
+  } else if (question.value === "nose") {
+    answerHeading.textContent = `The character has a ${Character.allCharactersArray[guessWho].nose} nose`;
+  } else if (question.value === "eyes") {
+    answerHeading.textContent = `The character has ${Character.allCharactersArray[guessWho].eyes} eyes`;
+  } else if (question.value === "facial-hair") {
+    if (Character.allCharactersArray[guessWho].facialHair) {
+      answerHeading.textContent = `${Character.allCharactersArray[guessWho].facialHair}, the character does have facial hair`;
+    } else {
+      answerHeading.textContent = `${Character.allCharactersArray[guessWho].facialHair}, the character does not have facial hair`;
+    }
+  } else if (question.value === "lips") {
+    answerHeading.textContent = `The character has ${Character.allCharactersArray[guessWho].lips} lips`;
+  } else if (question.value === "emotion") {
+    answerHeading.textContent = `The character is feeling ${Character.allCharactersArray[guessWho].emotion}`;
   }
+  guessResponse.classList.remove("opacity");
+  answerHeading.classList.add("opacity");
 }
+
+let guessOption;
 
 function renderGuessInput() {
   let guessOption = document.createElement("option");
@@ -275,14 +306,50 @@ function renderGuessInput() {
   }
 }
 
+document.getElementById("guess-btn").addEventListener("click", renderResponse);
+
+function renderResponse() {
+  guesses++;
+  if (guesses === maxGuessesAllowed) {
+    for (let i = 0; i < Character.allCharactersArray.length; i++) {
+      if (selectGuess.value === Character.allCharactersArray[guessWho].name) {
+        guessResponse.textContent = `Congratulations, you have guessed the who of guess who!`;
+      } else {
+        guessResponse.textContent = `Bad luck, that was you last attempt! The who of guess who was ${Character.allCharactersArray[guessWho].name}`;
+      }
+    }
+    if (selectGuess.value === Character.allCharactersArray[guessWho].name) {
+      guessWhoScore.push("win");
+      localStorage.setItem("guessWhoScore", JSON.stringify(guessWhoScore));
+    } else {
+      guessWhoScore.push("lose");
+      localStorage.setItem("guessWhoScore", JSON.stringify(guessWhoScore));
+    }
+  } else {
+    for (let i = 0; i < Character.allCharactersArray.length; i++) {
+      if (selectGuess.value === Character.allCharactersArray[guessWho].name) {
+        guessResponse.textContent = `Congratulations, you have guessed the who of guess who!`;
+      } else {
+        guessResponse.textContent = `Bad luck, ask another question to be given another clue!`;
+      }
+    }
+    if (selectGuess.value === Character.allCharactersArray[guessWho].name) {
+      guessWhoScore.push("win");
+      localStorage.setItem("guessWhoScore", JSON.stringify(guessWhoScore));
+    }
+  }
+
+  guessResponse.classList.add("opacity");
+  answerHeading.classList.remove("opacity");
+}
+
 const imgChar = document.getElementsByClassName("img-char");
 
 function renderCharacter() {
   for (let i = 0; i < Character.allCharactersArray.length; i++) {
-    let imgC = imgChar[i];
-    imgC.src = Character.allCharactersArray[i].src;
-    imgC.alt = Character.allCharactersArray[i].name;
+    Character.allCharactersArray[i].render();
   }
+  renderGWPlayerScore();
 }
 
 const imgName = document.getElementsByClassName("name-heading");
@@ -292,32 +359,6 @@ function renderName() {
     imgName[i].textContent = `${Character.allCharactersArray[i].name}`;
   }
 }
-
-hideBtn1.addEventListener("click", hideCharacter);
-hideBtn2.addEventListener("click", hideCharacter);
-hideBtn3.addEventListener("click", hideCharacter);
-hideBtn4.addEventListener("click", hideCharacter);
-hideBtn5.addEventListener("click", hideCharacter);
-hideBtn6.addEventListener("click", hideCharacter);
-hideBtn7.addEventListener("click", hideCharacter);
-hideBtn8.addEventListener("click", hideCharacter);
-hideBtn9.addEventListener("click", hideCharacter);
-hideBtn10.addEventListener("click", hideCharacter);
-hideBtn11.addEventListener("click", hideCharacter);
-hideBtn12.addEventListener("click", hideCharacter);
-
-showBtn1.addEventListener("click", showCharacter);
-showBtn2.addEventListener("click", showCharacter);
-showBtn3.addEventListener("click", showCharacter);
-showBtn4.addEventListener("click", showCharacter);
-showBtn5.addEventListener("click", showCharacter);
-showBtn6.addEventListener("click", showCharacter);
-showBtn7.addEventListener("click", showCharacter);
-showBtn8.addEventListener("click", showCharacter);
-showBtn9.addEventListener("click", showCharacter);
-showBtn10.addEventListener("click", showCharacter);
-showBtn11.addEventListener("click", showCharacter);
-showBtn12.addEventListener("click", showCharacter);
 
 function hideCharacter(event) {
   let hideButton = event.target.id;
@@ -343,3 +384,4 @@ function showCharacter(event) {
 renderGuessInput();
 renderCharacter();
 renderName();
+console.log(guessWho);
